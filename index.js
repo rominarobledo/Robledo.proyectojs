1d451d3c04c24148d4712b0f6ea5c78d30b7c7eb
romina.robledo@institutozonaoeste.edu.ar <116569246+rominarobledo@users.noreply.github.com>
2023-03-17 22:37:21 -0300

    e-commers pizzeria

--- index.js
+++ index.js
@@ -1,74 +1,144 @@
+/*
 
-// Proyecto hacer un menu para Pizzeria elija sabores y vaya sumando resuelva el total del pedido.
 
-const Muzzarela = 250;
-const Fugazzeta = 300;
-const Napolitana = 350;
-const Roquefort = 400;
-const Anchoas = 450;
-const Jamonymorron = 500;
+const muzzarela = 2500
+const fugazzeta = 3000
+const roquefort = 3500
+const napolitana = 4000
 
 
 
 
+let pizzaDeseada = parseInt(
+prompt('Escoge numero de sabor de pizza que desea comprar:\n 1.muzzarela\n 2.fugazzeta\n 3.roquefort\n 4.napolitana\n')
+)
+
+let escogioSabor = false
+
+let infopizza
 
 
 
-
-const mensajeMenu = `Menu:
-
-1. Muzzarela    4. Roquefort
-2. Fugazzeta  5. Anchoas
-3. Napolitana     6. Jamon y morron
-
-`;
-
-alert(mensajeMenu);
-
-let suma = 0;
-let cont = 0;
-
-
-let opcion = parseInt(prompt('Ingrese sabor de pizza'))
-while (opcion >= 1) {
-
-
-
-   if (opcion === 1) {
-      alert('Su total a abonar es:' + '' + Muzzarela)
-      opcion = parseInt(prompt('ingrese otro sabor de pizza o cero para finalizar'))
-
-   } else if (opcion === 2) {
-      alert('Su total a abonar es:  ' + '' + Fugazzeta)
-      opcion = parseInt(prompt('ingrese otro sabor de pizza o cero para finalizar'))
-
-   } else if (opcion === 3) {
-      alert('Su total a abonar es:' + '' + Napolitana)
-      opcion = parseInt(prompt('ingrese otro sabor de pizza o cero para finalizar'))
-   } else if (opcion === 4) {
-      alert('Su total a abonar es:' + '' + Roquefort)
-      opcion = parseInt(prompt('ingrese otro sabor de pizza o cero para finalizar'))
-   } else if (opcion === 5) {
-      alert('Su total a abonar es:' + '' + Anchoas)
-      opcion = parseInt(prompt('ingrese otro sabor de pizza o cero para finalizar'))
-   } else if (opcion === 6) {
-      alert('Su total a abonar es:' + '' + Jamonymorron)
-      opcion = parseInt(prompt('ingrese otro sabor de pizza o cero para finalizar'))
+while (escogioSabor === false) {
+   if (pizzaDeseada === 1) {
+      escogioSabor = true
+      infopizza = muzzarela
+   } else if (pizzaDeseada === 2) {
+      escogioSabor = true
+      infopizza = fugazzeta
+   } else if (pizzaDeseada === 3) {
+      escogioSabor = true
+      infopizza = roquefort
+   } else if (pizzaDeseada === 4) {
+      escogioSabor = true
+      infopizza = napolitana
    } else {
-      alert('La opcion ingresada no existe')
-      break;
+      pizzaDeseada = parseInt(
+         prompt('Escoge un numero de opcion correcta')
+      )
    }
- }
+
+
+}
+
+let cantidad = parseInt(prompt('ingrese cantidad'))
+let total = infopizza * cantidad
+alert('total es :'+ '' + total)
+*/
+
+/*Ejercicios de e-commer*/
+/*
+const productos  = [
+{
+   sabor: 'muzzarela'
+   precio: 2000
+   
+},
+{
+   sabor: 'fugazzeta'
+   precio: 2250
+   
+},
+{
+   sabor: 'picante'
+   precio: 2300
+   
+},
+{
+   sabor: 'pepperoni'
+   precio: 2350
+   
+},
+]
+*/
+
+
+// CLASE PRODUCTO
+
+class Producto {
+   constructor( id, nombre,precio){
+      this.nombre = nombre
+      this.id = id
+      this.precio = precio
+
+   }
+}
+
+
+// CRREAMOS CUATRO PRODUCTOS 
+const muzzarela = new Producto (1,'muzzarela',500)
+const  pepperoni = new Producto (2,'pepperoni',200)
+const fugazzeta = new Producto (3,'fugazzeta',1000)
+const margarita = new Producto (4,'margarita',1500)
+
+// GUARDAR PRODUCTOS
+const productos = [muzzarela,pepperoni,fugazzeta,margarita]
+console.log(productos)
+
+let productosEscogido = prompt ('Escoge el producto que deseas comprar: \n muzzarela\n pepperoni\n fugazzeta\n margarina\n')
+
+//VARIABLE PARA CONDICION DEL CICLO
+
+let seguirComprando = true
+const carrito = []
+
+while (seguirComprando === true){
+   //buscaar el producto escogido
+
+   const Producto = productos.find(
+      (Producto) => Producto.nombre === productosEscogido.toLocaleLowerCase().trim())
+// guardar producto en carrito o preguntarle al usuario un producto existente 
+   if (Producto){
+   carrito.push(Producto)
+}else{
+   productosEscogido = prompt(
+      'Escoge un producto correcto : \n muzzarela\n  pepperoni\n fugazzeta\n margarita\n')
+   
+      continue
+   
+}
+const decision = prompt ('Deseas seguir comprando ? si-no')
+
+if (decision === 'si') {
+productosEscogido = prompt (
+   'Escoge un producto correcto : \n muzzarela\n  pepperoni\n fugazzeta\n margarita\n'
+) 
+}else {
+   seguirComprando = false
+}
 
 
 
+}
 
+console.log (carrito);
+let totalCompra = 0
+for (const producto of carrito){
+   totalCompra = totalCompra + producto.precio
+}
+alert('el total de tu compra es: $'+totalCompra)
 
-
-
-
-
-
+alert(' ¡Muchas Gracias por su compra!')
 
 
 
